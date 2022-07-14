1740de8c9a7d2a51025ac82ebecf1297e2998aa9
KaspianDev <kacperleaguenine@gmail.com>
2022-07-14 18:00:24 +0200

    1.3
    - Decrease jar size
    - Fix plugin (lol)
    - Update deps

--- src/main/java/com/github/kaspiandev/dynamicpvparena/listeners/RegionListener.java
+++ src/main/java/com/github/kaspiandev/dynamicpvparena/listeners/RegionListener.java
@@ -1,7 +1,14 @@
 package com.github.kaspiandev.dynamicpvparena.listeners;
 
-import de.netzkronehd.wgregionevents.events.RegionEnterEvent;
-import de.netzkronehd.wgregionevents.events.RegionLeaveEvent;
+import static com.github.kaspiandev.dynamicpvparena.DynamicPVPArena.loadCachedKit;
+import static com.github.kaspiandev.dynamicpvparena.DynamicPVPArena.plugin;
+import static com.github.kaspiandev.dynamicpvparena.DynamicPVPArena.restoreInventory;
+import static com.github.kaspiandev.dynamicpvparena.DynamicPVPArena.saveInventory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -9,11 +16,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-
-import static com.github.kaspiandev.dynamicpvparena.DynamicPVPArena.*;
+import de.netzkronehd.wgregionevents.events.RegionEnteredEvent;
+import de.netzkronehd.wgregionevents.events.RegionLeaveEvent;
 
 
 public class RegionListener implements Listener {
@@ -22,21 +26,20 @@ public class RegionListener implements Listener {
     List<String> list = configuration.getStringList("regions");
 
     @EventHandler
-    public void onEnter(RegionEnterEvent event) throws IOException {
+    public void onEnter(RegionEnteredEvent event) throws IOException {
         Player player = event.getPlayer();
-        Bukkit.getConsoleSender().sendMessage("Player entered region");
         if (list.contains(event.getRegion().getId())) {
             saveInventory(player);
             player.getInventory().clear();
             loadCachedKit(player);
-            Bukkit.getConsoleSender().sendMessage("Player entered pvp region");
         }
     }
+
     @EventHandler
     public void onLeave(RegionLeaveEvent event) {
         Player player = event.getPlayer();
         if (list.contains(event.getRegion().getId())) {
             restoreInventory(player);
-		}
+        }
     }
 }
